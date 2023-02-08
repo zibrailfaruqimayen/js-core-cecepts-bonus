f47664163dff4b0052a6b6377f4a9c73c5789ae6
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-08 21:21:06 +0600

    simple

---
+++ factorial.js
@@ -0,0 +1,5 @@
+let factorial = 1;
+for(let i = 5; i >= 1; i++){
+   factorial = factorial * i;
+}
+console.log(factorial);
--- simple.js
+++ simple.js
@@ -5,8 +5,20 @@
 // console.log(sum);
 
 
-let sum = 0;
-for(let i = 5; i >= 1; i --){
-   sum = sum + 1;
+// let sum = 0;
+// for(let i = 5; i >= 1; i --){
+//    sum = sum + 1;
+// }
+//  console.log(sum);
+
+
+function sum(i){
+   if(i == 1){
+      return 1;
+   }
+   return i + sum(i-1);
 }
- console.log(sum);   
+
+const result = sum(5);
+console.log(result);
+   
