21d75b4be7f637c13e476c9f93d61b5b94836652
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-08 18:46:41 +0600

    simple

--- simple.js
+++ simple.js
@@ -1,5 +1,12 @@
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+//    sum = sum + i;
+// }
+// console.log(sum);
+
+
 let sum = 0;
-for(let i = 1; i <= 5; i++){
-   sum = sum + i;
+for(let i = 5; i >= 1; i --){
+   sum = sum + 1;
 }
-console.log(sum);
+ console.log(sum);   
